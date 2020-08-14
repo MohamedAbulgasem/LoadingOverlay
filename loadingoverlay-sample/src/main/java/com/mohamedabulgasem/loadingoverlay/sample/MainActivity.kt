2fec1bc911a89f7c2dd7ddfb2ead5a2bfa5c6fbd
Mohamed Abulgasem <mohamed.abulgasem@absa.africa>
2020-08-14 17:45:35 +0200

    Release v 1.0.0

--- loadingoverlay-sample/src/main/java/com/mohamedabulgasem/loadingoverlay/sample/MainActivity.kt
+++ loadingoverlay-sample/src/main/java/com/mohamedabulgasem/loadingoverlay/sample/MainActivity.kt
@@ -5,14 +5,14 @@ import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import com.mohamedabulgasem.loadingoverlay.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.*
 
 class MainActivity : AppCompatActivity() {
 
     private val loadingOverlay: LoadingOverlay by lazy {
         LoadingOverlay.with(
             context = this,
-            dimAmount = 0.8f
+            animation = LoadingAnimation.LOADING_SPINNER
         )
     }
 
@@ -21,48 +21,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         first_screen.setOnClickListener {
-            loadingOverlay.showFor(4, TimeUnit.SECONDS) {
+            loadingOverlay.showFor(4, SECONDS) {
                 val intent = Intent(this, SecondActivity::class.java)
                 startActivity(intent)
             }
         }
-    }
-
-    fun onCreateNewAccountButtonClicked() {
-        val loadingOverlay = LoadingOverlay.with(
-            context = this,
-
-            // Specify whether the overlay is cancelable on
-            // back presses or screen touches, default is false.
-            isCancellable = true,
-
-            onShowListener = {
-                // Optionally run some code when the overlay is shown.
-            },
-
-            onCancelListener = {
-                // Optionally run some code if isCancellable
-                // is set to true and the overlay is cancelled.
-            },
-
-            onDismissListener = {
-                // Optionally run some code when the overlay is dismissed.
-            }
-        )
-
-        // ...
-
-        // You can use the showFor function to only display the
-        // loading overlay for the specified period of time.
-        loadingOverlay.showFor(5, TimeUnit.SECONDS) {
-            // Optionally run some code after the set period has
-            // passed and the loading overlay has been dismissed.
-        }
-
-
-        loadingOverlay.show()
-        // ...
-        loadingOverlay.dismiss()
     }
 
 }
